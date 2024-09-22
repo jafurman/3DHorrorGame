7d409f664916bee629192ec81542ec69a47d880b
Joshua Furman <jafbird52@gmail.com>
2024-09-22 14:17:07 -0700

    Added in maze algorithm for map generation. Also added a 4 second sprint mechanic. Need to add interactions, enemies, and goal for base game

--- Assets/Scripts/PlayerMotor.cs
+++ Assets/Scripts/PlayerMotor.cs
@@ -6,10 +6,15 @@ public class PlayerMotor : MonoBehaviour
 {
     private CharacterController controller;
     private Vector3 playerVelocity;
-    public float speed = 5f;
+    public float speed = 1f;
     private bool isGrounded;
     public float gravity = -9.8f;
     public float jumpHeight = 3f;
+
+    public bool isSprinting = false;
+    public float sprintSpeed = 2f;
+    public float sprintDuration = 5f;
+    private float sprintTimer = 0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,13 +25,31 @@ public class PlayerMotor : MonoBehaviour
     void Update()
     {
         isGrounded = controller.isGrounded;
+
+        if (Input.GetKeyDown(KeyCode.LeftShift) && !isSprinting)
+        {
+            isSprinting = true;
+            sprintTimer = sprintDuration;
+        }
+
+        if (isSprinting)
+        {
+            sprintTimer -= Time.deltaTime;
+            if (sprintTimer <= 0)
+            {
+                isSprinting = false;
+            }
+        }
     }
     public void processMove(Vector2 input)
     {
         Vector3 moveDirection = Vector3.zero;
         moveDirection.x = input.x;
         moveDirection.z = input.y;
-        controller.Move(transform.TransformDirection(moveDirection) * speed * Time.deltaTime);
+
+        float currentSpeed = isSprinting ? sprintSpeed : speed;
+        controller.Move(transform.TransformDirection(moveDirection) * currentSpeed * Time.deltaTime);
+
         playerVelocity.y += gravity * Time.deltaTime;
         if (isGrounded && playerVelocity.y < 0)
         {
---
+++ Assets/mazeGenerator.cs
@@ -0,0 +1,150 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class mazeGenerator : MonoBehaviour
+{
+    [SerializeField]
+    private mazeCell mazeCellPrefab;
+
+    [SerializeField]
+    private int mazeWidth;
+
+    [SerializeField]
+    private int mazeDepth;
+
+    private mazeCell[,] mazeGrid;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mazeGrid = new mazeCell[mazeWidth, mazeDepth];
+
+        for (int x = 0; x < mazeWidth; x++)
+        {
+            for (int z = 0; z < mazeDepth; z++)
+            {
+                mazeGrid[x,z] = Instantiate(mazeCellPrefab, new Vector3(x, 0, z), Quaternion.identity);
+            }
+        }
+
+        GenerateMaze(null, mazeGrid[0,0]);
+    }
+
+    private void GenerateMaze(mazeCell previousCell, mazeCell currentCell)
+    {
+        currentCell.Visit();
+        ClearWalls(previousCell, currentCell);
+
+        new WaitForSeconds(0.05f);
+
+        mazeCell nextCell ;
+
+        do 
+        {
+
+        nextCell = GetNextUnvisitedCell(currentCell);
+
+        if (nextCell != null)
+        {
+            GenerateMaze(currentCell, nextCell);
+        }
+        } while (nextCell != null);
+    }
+
+    private mazeCell GetNextUnvisitedCell(mazeCell currentCell)
+    {
+        var unvisitedCells = GetUnvisitedCells(currentCell);
+
+        return unvisitedCells.OrderBy(_ => Random.Range(1,10)).FirstOrDefault();
+    }
+
+    private IEnumerable<mazeCell> GetUnvisitedCells(mazeCell currentCell)
+    {
+        int x = (int)currentCell.transform.position.x;
+        int z = (int)currentCell.transform.position.z;
+
+        if (x + 1 < mazeWidth)
+        {
+            var cellToRight = mazeGrid[x + 1, z];
+
+            if (cellToRight.isVisited == false)
+            {
+                yield return cellToRight;
+            }
+        }
+
+        if (x - 1 >= 0 )
+        {
+            var cellToLeft = mazeGrid[x - 1, z];
+
+            if (cellToLeft.isVisited == false)
+            {
+                yield return cellToLeft;
+            }
+        }
+
+        if (z + 1 < mazeDepth)
+        {
+            var cellToFront = mazeGrid[x,z + 1];
+
+            if (cellToFront.isVisited == false)
+            {
+                yield return cellToFront;
+            }
+        }
+
+        if (z - 1 >= 0)
+        {
+            var cellToBack = mazeGrid[x, z - 1];
+
+            if (cellToBack.isVisited == false)
+            {
+                yield return cellToBack;
+            }
+        }
+    }
+
+    private void ClearWalls(mazeCell previousCell, mazeCell currentCell)
+    {
+        if (previousCell == null)
+        {
+            return;
+        }
+
+        if (previousCell.transform.position.x < currentCell.transform.position.x)
+        {
+            previousCell.ClearRightWall();
+            currentCell.ClearLeftWall();
+            return;
+        }
+
+        if (previousCell.transform.position.x > currentCell.transform.position.x)
+        {
+            previousCell.ClearLeftWall();
+            currentCell.ClearRightWall();
+            return;
+        }
+
+        if (previousCell.transform.position.z < currentCell.transform.position.z)
+        {
+            previousCell.ClearFrontWall();
+            currentCell.ClearBackWall();
+            return;
+        }
+
+        if (previousCell.transform.position.z > currentCell.transform.position.z)
+        {
+            previousCell.ClearBackWall();
+            currentCell.ClearFrontWall();
+            return;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
